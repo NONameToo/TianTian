b441041b59455f52de4e9be2d823f0fee161a980
xwp <1076796244@qq.com>
2018-09-06 08:58:07 +0800

    基础框架

---
+++ tiantian/views.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, 'tiantian/index.html')
+
+
+def cart(request):
+    return render(request, 'tiantian/cart.html')
+
+
+def detail(request):
+    return render(request, 'tiantian/detail.html')
+
+
+def list(request):
+    return render(request, 'tiantian/list.html')
+
+
+def login(request):
+    return render(request, 'tiantian/login.html')
+
+
+def place_order(request):
+    return render(request, 'tiantian/place_order.html')
+
+
+def register(request):
+    return render(request, 'tiantian/register.html')
+
+
+def user_center_info(request):
+    return render(request, 'tiantian/user_center_info.html')
+
+
+def user_center_order(request):
+    return render(request, 'tiantian/user_center_order.html')
+
+
+def user_center_site(request):
+    return render(request, 'tiantian/user_center_site.html')
